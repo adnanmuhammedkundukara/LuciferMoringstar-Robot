c8ae0abfdebe1fc99bec3075ed61a04ff466cde8
ADNAN <73103641+adnanmuhammedkundukara@users.noreply.github.com>
2022-09-24 17:12:54 +0530

    Update autofilter_func.py

--- LuciferMoringstar_Robot/functions/autofilter_func.py
+++ LuciferMoringstar_Robot/functions/autofilter_func.py
@@ -20,9 +20,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# Telegram Link : https://telegram.dog/Mo_Tech_Group
-# Repo Link : https://github.com/PR0FESS0R-99/LuciferMoringstar-Robot
-# License Link : https://github.com/PR0FESS0R-99/LuciferMoringstar-Robot/blob/LuciferMoringstar-Robot/LICENSE
+# Telegram Link : https://telegram.dog/ebooks_malayalam_ebooks
 
 def split_list(l, n):
     for i in range(0, len(l), n):
